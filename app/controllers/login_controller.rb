e3dc5ed224befca60e1dbbb1021f5bbd029573c9
Gerry Hernandez <gerry_hd@live.com.mx>
2018-01-26 12:34:12 -0600

    Se agregó el helper de login

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -23,4 +23,5 @@ class LoginController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to index_path
+  end
 end
---
+++ app/helpers/login_helper.rb
@@ -0,0 +1,41 @@
+module LoginHelper
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    user_id = session[:user_id]
+    @current_user ||= User.find_by(id: user_id)
+  end
+
+  def log_in(user)
+    session[:user_id] = user.id
+    session[:user_role] = user.type_user.name
+    current_user
+  end
+
+  def log_out
+    session.delete(:user_id)
+    session.delete(:user_role)
+    @current_user = nil
+  end
+
+  def enter
+    if administrator?
+      # redirect
+    end
+
+    if applicant?
+      # reditect
+    end
+
+  end
+
+  def administrator?
+    current_user.type_user.id == TypeUser::ADMIM
+  end
+
+  def applicant?
+    current_user.type_user.id == TypeUser::APPLICANT
+  end
+end
