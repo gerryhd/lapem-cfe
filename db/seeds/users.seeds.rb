cacde2148d48e81c0c2391329d2ab3492a9da08e
Gerry Hernandez <gerry_hd@live.com.mx>
2018-01-29 12:47:52 -0600

    Se agregó el modelo de Applicant

--- db/seeds/users.seeds.rb
+++ db/seeds/users.seeds.rb
@@ -1,7 +1,18 @@
 after :type_users do
+
+  # Create test admin user
+
   admin_user_type = TypeUser.find(TypeUser::ADMIN)
 
   admin_user = User.create(username: 'admin2018', password: '2018', password_confirmation: '2018', type_user: admin_user_type)
 
   Administrator.create(user: admin_user)
+
+  # Create test applicant user
+
+  applicant_user_type = TypeUser.find(TypeUser::APPLICANT)
+
+  applicant_user = User.create(username: 'app2018', password: '2018', password_confirmation: '2018', type_user: applicant_user_type)
+
+  Applicant.create(user: applicant_user, email: 'test@cubicode.mx')
 end
