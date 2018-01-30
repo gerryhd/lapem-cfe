52e6f869fec56ae38dabbe2b03efec1332aed05f
Gerry Hernandez <gerry_hd@live.com.mx>
2018-01-30 12:26:20 -0600

    Se agregó módulo de registro de aplicantes con usuario

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
     scope module: 'applicant' do
       get 'index' => 'index#index', as: 'applicant_index'
+      get 'register' => 'index#new', as: 'new_applicant'
+      post 'register' => 'index#create'
     end
     
   end
