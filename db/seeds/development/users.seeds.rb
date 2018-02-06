3fa2b5e2f74df85e627365b2b28862a503016197
Gerry Hernandez <gerry_hd@live.com.mx>
2018-02-06 01:36:05 -0600

    emergency

--- db/seeds/development/users.seeds.rb
+++ db/seeds/development/users.seeds.rb
@@ -12,7 +12,8 @@ applicant_user_type = TypeUser.find(TypeUser::APPLICANT)
   last_name = Faker::Name.last_name
   phone = Faker::PhoneNumber.phone_number
   email = Faker::Internet.unique.email
-  position = Faker::Job.title
+  byebug
+  position = Faker::Job.position
   
   user = User.create(username: username, password: password, password_confirmation: password)
   Applicant.create(user: user, email: email, name: name, last_name: last_name, phone: phone, position: position)
