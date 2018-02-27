5c6af5b48feb92d665fb0177ab0b909539bd49dc
Gerardo Hernández <ghernandez@cubicode.mx>
2018-02-27 00:13:27 -0600

    Se agregaron opciones deaprobar y rechazar junto con vistas de detalles de la aplicación

--- app/admin/applications.rb
+++ app/admin/applications.rb
@@ -9,8 +9,6 @@ ActiveAdmin.register Application do
   filter :status, as: :select, collection: Application.statuses_t, filters: ['eq']
 
   index do
-
-    
     column :applicant do |application|
       link_to application.applicant.full_name, admin_applicant_path(application.applicant.id)
     end
@@ -22,22 +20,94 @@ ActiveAdmin.register Application do
     end
     column "Creado el", :created_at
 
-    actions
+    actions defaults: true do |app|
+      text_node link_to "Aprobar", approve_admin_application_path(app), method: :put, class: "member_link" if app.pending?
+      text_node link_to "Rechazar", reject_admin_application_path(app), method: :put, class: "member_link" if app.pending?
+    end
   end
+
+  member_action :approve, method: :put do
+    resource.approved!
+    flash[:success] = "La aplicación fue aprobada."
+
+    redirect_to admin_applications_path
+  end
+
+  member_action :reject, method: :put do
+    resource.rejected!
+    flash[:success] = "La aplicación ha sido rechazada."
+
+    redirect_to admin_applications_path
+  end
+
 
   show do
     attributes_table do
       row :applicant
-      row :status
+      row :status do |app|
+        I18n.t("status.#{app.status.to_s}")
+      end
       row :application_type
+    end
 
-      row :brand do |app|
-        # 
+    if application.application_type_id == ApplicationType::BRAND
+      
+      panel "Registro de Marca" do
+        attributes_table_for application.brand do
+          row :brand_type
+          row :sign_type
+        end
+      end
+
+      panel "Persona" do
+        person = application.brand.person
+        
+
+        if person.instance_of? NaturalPerson
+          h1 "Persona física"
+          attributes_table_for person do
+            row :date
+            row :name
+            row :last_name
+            row :second_last_name
+            row :nationality
+            row :phone_number
+            row :email
+          end
+        elsif person.instance_of? LegalPerson
+          h1 "Persona moral"
+          attributes_table_for person do
+            row :date
+            row :denomination
+            row :rfc
+            row :phone_number
+            row :email
+          end
+        end
+        
+      end
+    
+    elsif application.application_type_id == ApplicationType::PATENT
+
+      panel "Patente" do
+        attributes_table_for application.patent do
+          row :title
+          row :divulgation_date
+        end
+      end
+
+    elsif application.application_type_id == ApplicationType::COPYRIGHT
+
+      panel "Derechos de Autor" do
+        attributes_table_for application.patent do
+          row :title
+          row :summary
+        end
       end
 
     end
+    active_admin_comments
   end
-
   
   controller do
     let :admin, :all
@@ -50,6 +120,7 @@ ActiveAdmin.register Application do
       if query.blank?
         super
       else
+
         # If query matches any of the statuses, return the scope
         (I18n.t :status).each do |k,s|
           if s.casecmp(query) == 0
