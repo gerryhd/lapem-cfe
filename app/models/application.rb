6b89c6af208801528613a6134cfa1f3e0a7aa08a
Gustavo <gcanul@cubicode.mx>
2018-03-06 11:16:45 -0600

    Nuevos modelos

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,18 +4,12 @@ class Application < ActiveRecord::Base
   
   belongs_to :applicant
   belongs_to :application_type
-
-  has_one :brand
-  has_one :patent
-  has_one :copyright
   belongs_to :status_application
+
+  belongs_to :applicable, polymorphic: true
 
   
   validates_presence_of :application_type, :applicant
-
-  scope :brands, ->{ where(application_type_id: ApplicationType::BRAND) }
-  scope :patents, ->{ where(application_type_id: ApplicationType::PATENT) }
-  scope :copyrights, ->{ where(application_type_id: ApplicationType::COPYRIGHT) }
 
 
   def self.statuses_t
--- app/models/distinctive_sign.rb
+++ app/models/distinctive_sign.rb
@@ -1,4 +1,5 @@
 class DistinctiveSign < ActiveRecord::Base
   belongs_to :sign_type
+  has_one :application, as: :applicable
 
 end
