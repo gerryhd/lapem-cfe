d57136436947ac11967602669fa34854bf60b276
Gerardo Hernández <ghernandez@cubicode.mx>
2018-03-13 12:20:36 -0600

    Se corrigió la visualización del logo en el módulo de admin y se agregaron gráfias de pastel al inicio del módulo

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,15 +1,22 @@
 ActiveAdmin.register_page "Dashboard" do
+  menu priority: 1, label: "Inicio"
 
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
-
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  content title: "Inicio" do
+    panel "Estadísticas de solicitudes por estado" do
+      pie = Hash.new
+      StatusApplication.all.each do |status|
+        pie[status.name] = Application.where(status_application_id: status.id).count
       end
+      pie_chart pie
     end
 
+    panel "Estadísticas de solicitudes por tipo" do
+      pie = Hash.new
+      ApplicationType.all.each do |type|
+        pie[type.name] = Application.where(application_type_id: type.id).count
+      end
+      pie_chart pie
+    end
     # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
