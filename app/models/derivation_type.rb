ab22cdaabdeaf22819a7eba89413aa8522f51f5e
Gerardo Hernández <ghernandez@cubicode.mx>
2018-03-16 15:16:31 -0600

    Se agregaron seeds de tipo de derivación

--- app/models/derivation_type.rb
+++ app/models/derivation_type.rb
@@ -1,2 +1,11 @@
 class DerivationType < ActiveRecord::Base
+  EXTENSION = 1
+  FIX = 2
+  ADAPTATION = 3
+  COMPILATION = 4
+  COLLECTION = 5
+  TRANSLATION = 6
+  COMPENDIUM = 7
+  PARAPHRASE = 8
+  TRANSFORMATION = 9
 end
