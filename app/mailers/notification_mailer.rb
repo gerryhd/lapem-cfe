6e7ad1eaff9259593d5d35815de26d1fa96e2c93
Gerry Hernandez <gerry_hd@live.com.mx>
2018-08-21 11:57:21 -0500

    Notificaciones por correo

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -7,4 +7,16 @@ class NotificationMailer < ApplicationMailer
     mail(to: @user.applicant.email, subject: 'Nueva Solicitud')
   end
 
+  def application_updated(user, application)
+    @user = user
+    @application = application
+    mail(to: @user.applicant.email, subject: 'Actualización del estado de la solicitud')
+  end
+
+  def application_modified(user, application)
+    @user = user
+    @application = application
+    mail(to: @user.applicant.email, subject: 'Solicitante ha modificado su solicitud')
+  end
+
 end
